726e7b7434cbd65fae4463451eb8322f9bed1176
yuuya-1205 <64653733+yuuya-1205@users.noreply.github.com>
2023-07-29 12:02:23 +0900

    colorを共通コンポーネントとしてutil作成

---
+++ lib/util/color.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const primary = Color.fromRGBO(31, 162, 203, 1);
+const disPrimary = Color.fromRGBO(254, 197, 111, 1);
+const fillGray = Color.fromRGBO(240, 240, 240, 1);
+const textGray = Color.fromRGBO(131, 124, 124, 1);
